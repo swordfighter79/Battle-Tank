37e8d88f8c5619d173e69f53ca013481c6188ee8
Robert <swordfighter79@gmail.com>
2018-10-29 14:08:59 -0400

    Battle Tank  127 Commit  C++ coding Tank, TankPlayercontroller

---
+++ Battle_Tank/Source/Battle_Tank/Private/TankPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+#include "Battle_Tank.h"
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
