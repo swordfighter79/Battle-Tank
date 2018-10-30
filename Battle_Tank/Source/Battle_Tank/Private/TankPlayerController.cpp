d29a58673dff894af998f51819cf24aebfc11214
Robert <swordfighter79@gmail.com>
2018-10-30 11:23:36 -0400

    Battle Tank 133 Commit  add tick to player controller

--- Battle_Tank/Source/Battle_Tank/Private/TankPlayerController.cpp
+++ Battle_Tank/Source/Battle_Tank/Private/TankPlayerController.cpp
@@ -3,6 +3,11 @@
 #include "TankPlayerController.h"
 #include "Battle_Tank.h"
 
+
+//  Tick
+	// Super
+
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -19,7 +24,24 @@ void ATankPlayerController::BeginPlay()
 }
 
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);	
+	AimTowardsCrosshair();
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	//  Get world location if linetrace through crosshair
+	//  if it hits the landscape
+		//  tell controlled tank to aim at this point
 }
